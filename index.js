e35487a6d6e40b6ace94b85edfedd05f932fb496
Badhri <badhrisambath2003@gmail.com>
2024-08-11 21:56:12 +0530

    Updated CORS

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express = require("express")
-const { Router } = require('./routes/expenseRoute')
+const express = require("express");
+const { Router } = require('./routes/expenseRoute');
 const mongoose = require("mongoose");
-
-
-const app = express()
 const cors = require("cors");
 
-app.use(cors(["https://money-matrix-frontend.vercel.app/"]))
-app.use(express.json())
+const app = express();
+
+// Correctly configure CORS
+app.use(cors({ origin: "https://money-matrix-frontend.vercel.app" }));
+app.use(express.json());
 app.use('/', Router);
 
-
 mongoose.connect("mongodb+srv://Badhri007:Badlak717@money-matrix.0i3z4s6.mongodb.net/").then(() => {
-    console.log("Mongo db connected..")
+    console.log("Mongo db connected..");
 });
 
 app.listen(5000, () => {
-    console.log("Server started 123...");
-})
+    console.log("Server started...");
+});
