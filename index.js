83b5aa1607c9005000a56d8549e17e40833a1c91
Badhri <badhrisambath2003@gmail.com>
2024-08-11 22:03:22 +0530

    Updated CORS -3

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 
 // Correctly configure CORS
 const corsOptions = {
-    origin: "https://money-matrix-frontend.vercel.app",
+    origin: "https://money-matrix-frontend.vercel.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204
@@ -37,10 +37,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
-});
+  });
 
 app.options('*', cors(corsOptions)); // Enable pre-flight for all routes
 
