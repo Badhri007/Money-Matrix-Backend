76e557243aba5df396e49c24a0dc187c30818505
Badhri <badhrisambath2003@gmail.com>
2024-08-11 22:00:16 +0530

    Updated CORS -3

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://money-matrix-frontend.vercel.app");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
