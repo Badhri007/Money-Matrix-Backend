242c4a73df52a794bafe8a40d11f298b83873627
Badhri <badhrisambath2003@gmail.com>
2024-08-11 21:58:04 +0530

    Updated CORS -2

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+// const express = require("express");
+// const { Router } = require('./routes/expenseRoute');
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+
+// const app = express();
+
+// // Correctly configure CORS
+// app.use(cors({ origin: "https://money-matrix-frontend.vercel.app" }));
+// app.use(express.json());
+// app.use('/', Router);
+
+// mongoose.connect("mongodb+srv://Badhri007:Badlak717@money-matrix.0i3z4s6.mongodb.net/").then(() => {
+//     console.log("Mongo db connected..");
+// });
+
+// app.listen(5000, () => {
+//     console.log("Server started...");
+// });
 const express = require("express");
 const { Router } = require('./routes/expenseRoute');
 const mongoose = require("mongoose");
@@ -8,12 +27,21 @@ const app = express();
 // Correctly configure CORS
 app.use(cors({ origin: "https://money-matrix-frontend.vercel.app" }));
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://money-matrix-frontend.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 app.use('/', Router);
 
 mongoose.connect("mongodb+srv://Badhri007:Badlak717@money-matrix.0i3z4s6.mongodb.net/").then(() => {
-    console.log("Mongo db connected..");
+    console.log("MongoDB connected...");
+}).catch(err => {
+    console.error("MongoDB connection error:", err);
 });
 
 app.listen(5000, () => {
-    console.log("Server started...");
+    console.log("Server started on port 5000...");
 });
